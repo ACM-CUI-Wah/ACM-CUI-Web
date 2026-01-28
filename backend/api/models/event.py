270fa0d70ba1aa5ef9f2e37453e1c62b494905ec
NextStep-IM <malikharis2003@gmail.com>
2026-01-29 02:46:15 +0500

    Add models for Event Registration module

--- backend/api/models/event.py
+++ backend/api/models/event.py
@@ -2,17 +2,29 @@ from django.db import models
 from datetime import date
 from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
+from django.core.validators import RegexValidator
 
 
 def event_image_upload_path(instance, filename):
     return f'events/{instance.id}/{filename}'
 
-class EventType(models.Model):
 
+class RegistrationType(models.TextChoices):
+    SINGLE = 'SINGLE'
+    TEAM = 'TEAM'
+
+
+class RegistrationStatus(models.TextChoices):
+    PENDING = 'PENDING'
+    COMPLETED = 'COMPLETED'
+    CANCELLED = 'CANCELLED'
+
+
+class EventType(models.Model):
     type = models.CharField(max_length=50, unique=True)
 
     class Meta:
-        ordering =['type']
+        ordering = ['type']
         verbose_name = 'Event Type'
         verbose_name_plural = 'Event Types'
 
@@ -24,8 +36,8 @@ class EventType(models.Model):
     def __str__(self):
         return self.type
 
+
 class Event(models.Model):
-    
     event_type = models.ForeignKey(EventType, on_delete=models.PROTECT, related_name='events', null=True)
 
     title = models.CharField(max_length=200)
@@ -35,7 +47,8 @@ class Event(models.Model):
     time_from = models.TimeField()
     time_to = models.TimeField()
     location = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to='events/', default=f'{settings.MEDIA_ROOT}/events/default.png', blank=True, null=True)
+    image = models.ImageField(upload_to='events/', default=f'{settings.MEDIA_ROOT}/events/default.png', blank=True,
+                              null=True)
     total_seats = models.PositiveIntegerField(default=0)
     tags = ArrayField(
         models.CharField(
@@ -65,6 +78,62 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class EventRegistration(models.Model):
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name='registrations'
+    )
+
+    registration_type = models.CharField(
+        max_length=10,
+        choices=RegistrationType.choices
+    )
+
+    team_name = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=RegistrationStatus.choices,
+        default=RegistrationStatus.PENDING
+    )
+
+    def __str__(self):
+        return f"{self.event.title} - {self.registration_type}"
+
+
+class EventParticipant(models.Model):
+    registration = models.ForeignKey(
+        EventRegistration,
+        on_delete=models.CASCADE,
+        related_name='participants'
+    )
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    # reg_no_regex = RegexValidator(
+    #     regex=r'^(?:FA|SP)[0-9]{2}-B(?:CS|AI|SE)-[0-9]{3}$',
+    #     message='Enter a valid registration number (e.g, FA22-BCS-012).',
+    # )
+    reg_no = models.CharField(max_length=30)
+    current_semester = models.PositiveSmallIntegerField()
+    department = models.CharField(max_length=100)
+    # phone_regex = RegexValidator(
+    #     regex=r'^\+92[0-9]{10}$',
+    #     message="Enter a valid Pakistani number in the format +92XXXXXXXXXX.",
+    # )
+    phone_no = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.name
+
 
 # NOTE: Only remove this when serializers and views are finished
 class EventImage(models.Model):
