3d7de7d92ca16a72812cd0892172e80f00797ef6
NextStep-IM <malikharis2003@gmail.com>
2026-02-08 02:30:21 +0500

    Set up bucket storage for bills

--- backend/api/models/bill.py
+++ backend/api/models/bill.py
@@ -10,7 +10,7 @@ class Bill(models.Model):
     description = models.TextField(max_length=200)
     amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     date = models.DateField(default=date.today)
-    image = models.ImageField(upload_to=bill_image_upload_path)
+    image = models.CharField(max_length=255)
 
     def save(self, *args, **kwargs):
         if self.id is None:
--- backend/api/serializers/bill.py
+++ backend/api/serializers/bill.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
 from api.models import Bill
+from api.utils import get_bucket_public_url, upload_file
 
 class BillSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Bill
         fields = '__all__'
         read_only_fields = ['id']
+
+    def get_image_url(self, obj):
+        if not obj.image:
+            return None
+        return get_bucket_public_url(obj.file)
+
+    def create(self, validated_data):
+        image = validated_data.pop("image", None)
+        bill = Bill.objects.create(**validated_data)
+
+        if image:
+            bill.image = upload_file(image, "bills")
+            bill.save(update_fields=["image"])
+
+        return bill
