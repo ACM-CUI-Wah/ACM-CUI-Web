0f7512f94720589ddb6db743d2491d42f5779485
dev-hafeez <159642563+dev-hafeez@users.noreply.github.com>
2026-02-08 18:29:14 +0500

    Initialize API module with view imports

---
+++ backend/api/__init__.py
@@ -0,0 +1,11 @@
+from .admin import AdminRUDView
+from .auth import SignupView, OTPView, LoginView, LogoutView, PasswordChangeView
+from .bill import BillRUDView, BillListCreateView
+from .blog import BlogEditView, BlogDeleteView, BlogUploadView, InlineImageUploadView, BlogListAPIView, BlogDetailView
+from .event import EventDetailView, EventTypeListCreateView, EventListCreateView, EventRegistrationListCreateView, RegistrationStatusUpdateView, EventRegistrationDeleteView, EventRegistrationDetailView
+from .meeting import MeetingPDFView, MeetingListView, MeetingRUDView, MeetingCreateView, MeetingAttendanceRUDView, \
+    MeetingAttendanceListView
+from .root import api_root
+from .user import StudentRUView, StudentsListView, PublicStudentsListView
+from .recruitment import RecruitmentSessionViewSet, ApplicationReviewViewSet, ApplicationStatusUpdateViewSet, \
+    ApplicationSubmitView, ActiveRecruitmentSessionView, RecruitmentApplicationsExcelView
